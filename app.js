b9ee3d3bae835efb5f55bfb5e205c2d9c0f1b578
Petro Soininen <petro.soininen@microsoft.com>
2017-04-15 21:55:49 +0100

    bot

--- app.js
+++ app.js
@@ -1,46 +1,28 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var restify = require('restify');
+var builder = require('botbuilder');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+//=========================================================
+// Bot Setup
+//=========================================================
 
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', index);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url); 
 });
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  
+// Create chat bot
+var connector = new builder.ChatConnector({
+    appId: myappid,
+    appPassword: mypw
 });
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
 
-module.exports = app;
+//=========================================================
+// Bots Dialogs
+//=========================================================
+
+bot.dialog('/', function (session) {
+    session.send("Hello World");
+});
